c9596ec749f4b222f537d0ff2a6bd151c4a79a37
Denis Babenko <babenko.denis3009@gmail.com>
2020-09-18 20:55:58 +0300

    Added min validator

--- game/models.py
+++ game/models.py
@@ -1,10 +1,11 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
 class ScoreRecord(models.Model):
     """Record in rating tables"""
     name = models.CharField(blank=False, max_length=20, db_index=True)
-    score = models.IntegerField(blank=False, default=0, db_index=True)
+    score = models.IntegerField(blank=False, default=0, db_index=True, validators=[MinValueValidator(0)])
 
     def __str__(self):
         return f'{self.name}: {self.score}'
